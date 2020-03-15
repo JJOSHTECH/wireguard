a0c9eb1e4d30b41516b23773f5bc2c4c6f0f017f
JJOSH.TECH <46561533+JJOSHTECH@users.noreply.github.com>
2020-03-16 00:31:17 +0100

    Update startUp.sh

--- startUp.sh
+++ startUp.sh
@@ -7,11 +7,14 @@ ip address add dev wg0 192.168.200.1/24
 # Create Private Key
 mkdir -p /etc/keys
 umask 077
-wg genkey > /etc/keys/private
-wg pubkey < /etc/keys/private > /etc/keys/public
+if [ "$(ls -l /etc/keys)" = "total 0" ]
+  then 
+    wg genkey > /etc/keys/private
+    wg pubkey < /etc/keys/private > /etc/keys/public
+fi
 
 # Create Wireguard Server
-wg set wg0 listen-port $LISTEN_PORT private-key $PRIVATE_KEY peer $PEER allowed-ips $ALLOWED_IPS endpoint $ENDPOINT
+wg set wg0 listen-port $LISTEN_PORT private-key /etc/keys/private peer /etc/keys/public allowed-ips $ALLOWED_IPS endpoint $ENDPOINT
 
 # Set Interface up
 ip link set up dev wg0
